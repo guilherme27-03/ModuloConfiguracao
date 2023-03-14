8958463551e6f02f5b47c505dec56172f5eee237
guilherme27-03 <115494518+guilherme27-03@users.noreply.github.com>
2023-03-14 16:13:40 -0300

    Atualizacao

---
+++ Configuracao/Models/Permissao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Permissao
+    {
+        public string Descricao { get; set; }
+        public int Id { get; set; }   
+        public List<GrupoUsuario> Grupo { get; set; }
+    }
+}
---
+++ Configuracao/Models/Usuário.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Models
+{
+    public class Usuário
+
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NomeUsuario { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public bool Ativo { get; set; }
+        public string Senha { get; set; }
+        public  List<GrupoUsuario> GrupoUsuarios { get; set; }
+    }
+}
