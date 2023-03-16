3618221de3c221c9bd462d8644cb04fb3c3ffd1d
guilherme27-03 <115494518+guilherme27-03@users.noreply.github.com>
2023-03-16 14:22:45 -0300

    Atualization

--- Configuracao/DAL/UsuarioDAL.cs
+++ Configuracao/DAL/UsuarioDAL.cs
@@ -38,7 +38,7 @@ namespace DAL
             }
         }
 
-        public List<Usuario> BuscarPorNome()
+        public List<Usuario> BuscarPorNome(string _nome)
         {
             List<Usuario> usuarios = new List<Usuario>();
             Usuario usuario = new Usuario();
@@ -47,11 +47,35 @@ namespace DAL
             {
                 SqlCommand cmd = new SqlCommand();
                 cmd.Connection = cn;
-                cmd.CommandText = "SELECT Id,Nome,NomeUsuario,Email,Cpf,Ativo,Senha WHERE " ;
-            }
-            catch
-            {
+                cmd.CommandText = "SELECT Id,Nome,NomeUsuario,Email,Cpf,Ativo,Senha FROM Usuario WHERE Nome LIKE @Nome";
+                cmd.Parameters.AddWithValue("@Nome", "%" + _nome + "%");
 
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    if (rd.Read())
+                    {
+                        usuario = new Usuario();
+                        usuario.Id = Convert.ToInt32(rd["ID"]);
+                        usuario.Nome = rd["Nome"].ToString();
+                        usuario.NomeUsuario = rd["NomeUsuario"].ToString();
+                        usuario.Email = rd["Email"].ToString();
+                        usuario.CPF = rd["CPF"].ToString();
+                        usuario.Ativo = Convert.ToBoolean(rd["Ativo"]);
+                        usuario.Senha = rd["Senha"].ToString();
+                        usuarios.Add(usuario);
+                    }
+                }
+                return usuarios;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar Id de usuario no banco de dados", ex);
+            }
+            finally
+            {
+                cn.Close();
             }
 
         }
@@ -66,13 +90,13 @@ namespace DAL
                 SqlCommand cmd = new SqlCommand();
                 cmd.Connection = cn;
                 cmd.CommandText = "SELECT Id,Nome,NomeUsuario,Email,CPF,Ativo,Senha WHERE (@Id,@Nome,@NomeUsuario,@Email,@CPF,@Ativo,@Senha)";
-                cmd.CommandType= System.Data.CommandType.Text;
+                cmd.CommandType = System.Data.CommandType.Text;
 
                 cn.Open();
 
                 using (SqlDataReader rd = cmd.ExecuteReader())
                 {
-                    while(rd.Read())
+                    while (rd.Read())
                     {
                         usuario = new Usuario();
                         usuario.Id = Convert.ToInt32(rd["ID"]);
@@ -83,17 +107,19 @@ namespace DAL
                         usuario.Ativo = Convert.ToBoolean(rd["Ativo"]);
                         usuario.Senha = rd["Senha"].ToString();
                         usuarios.Add(usuario);
-                    }  
+                    }
                 }
                 return usuarios;
             }
             catch (Exception ex)
             {
-                throw new Exception("Ocorreu um erro ao tentar buscar todos os usuarios no banco de dados",ex);
+                throw new Exception("Ocorreu um erro ao tentar buscar todos os usuarios no banco de dados", ex);
             }
             finally
             {
                 cn.Close();
+
+
             }
         }
         public void BuscarPorNomeUsuario(string _nomeUsuario)
@@ -131,6 +157,7 @@ namespace DAL
                 }
 
             }
+
             catch (Exception ex)
             {
                 throw new Exception("Ocorreu um erro ao tentar buscar Nome de usuário no banco de dados", ex);
@@ -184,10 +211,59 @@ namespace DAL
             {
                 cn.Close();
             }
-            
-            
+
+
         }
-        public void Alterar(Usuario _usuario)
+
+        public void BuscarPorCpf(string _cpf)
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+            Usuario usuario = new Usuario();
+
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "SELECT Id,Nome,NomeUsuario,Email,CPF,Ativo,Senha WHERE CPF = @CPF";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@CPF+.", _cpf);
+
+                cn.Open();
+
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        usuario = new Usuario();
+                        usuario.Id = Convert.ToInt32(rd["ID"]);
+                        usuario.Nome = rd["Nome"].ToString();
+                        usuario.NomeUsuario = rd["NomeUsuario"].ToString();
+                        usuario.Email = rd["Email"].ToString();
+                        usuario.CPF = rd["CPF"].ToString();
+                        usuario.Ativo = Convert.ToBoolean(rd["Ativo"]);
+                        usuario.Senha = rd["Senha"].ToString();
+                        usuarios.Add(usuario);
+                    }
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar buscar Cpf no banco de dados", ex);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+   
+
+            public void Alterar(Usuario _usuario)
         {
             SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
             try
@@ -213,7 +289,7 @@ namespace DAL
                 cn.Close();
             }
         }
-        public void  Excluir(int _id)
+        public void Excluir(int _id)
         {
             SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
             try
@@ -221,8 +297,8 @@ namespace DAL
                 SqlCommand cmd = cn.CreateCommand();
                 cmd.CommandText = "DELETE FROM Usuario WHERE ID = @Id";
                 cmd.CommandType = System.Data.CommandType.Text;
-                cmd.Parameters.AddWithValue("@Id",_id);
-                
+                cmd.Parameters.AddWithValue("@Id", _id);
+
                 cmd.Connection = cn;
             }
             catch (Exception ex)
@@ -235,4 +311,5 @@ namespace DAL
             }
         }
     }
-}
+  }
+
