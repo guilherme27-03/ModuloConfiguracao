3c88779d970c60eff308a73f11cf2771469e5f4d
guilherme27-03 <115494518+guilherme27-03@users.noreply.github.com>
2023-03-17 17:22:44 -0300

    Atualization

--- Configuracao/BLL/GrupoUsuarioBLL.cs
+++ Configuracao/BLL/GrupoUsuarioBLL.cs
@@ -1,5 +1,6 @@
 ﻿using DAL;
 using Models;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 
 namespace BLL
@@ -15,6 +16,24 @@ namespace BLL
         public void Excluir(int id)
         {
             new GrupoUsuarioDAL().Excluir(id);
+        }
+        public List<GrupoUsuario> BuscarTodos()
+        {
+            return new GrupoUsuarioDAL().BuscarTodos();
+        }
+        public List<GrupoUsuario> BuscarPorNomeGrupo(string _nomegrupo)
+        {
+            return new GrupoUsuarioDAL().BuscarPorNomeGrupo(_nomegrupo);
+        }
+        public GrupoUsuario BuscarPorId(int _id)
+        {
+            return new GrupoUsuarioDAL().BuscarPorId(_id);
+        }
+        public void Alterar(GrupoUsuario _grupousuario)
+        {
+            ValidarDados(_grupousuario);
+
+            new GrupoUsuarioDAL().Alterar(_grupousuario);
         }
         private void ValidarDados(GrupoUsuario _grupousuario)
         {
